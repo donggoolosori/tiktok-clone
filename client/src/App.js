682af3e01f7af4150300e4d854803df9e777d86c
donggoolosori <kdj6551@naver.com>
2020-12-25 22:50:44 +0900

    Connect frontend and backemd

--- client/src/App.js
+++ client/src/App.js
@@ -18,24 +18,19 @@ function App() {
   return (
     <div className="app">
       <div className="app__videos">
-        <Video
-          url="https://user-images.githubusercontent.com/53213397/103125802-ac6e0c80-46cf-11eb-9cac-2ed4f4caf7e7.mp4"
-          channel="aaaa"
-          description="MERN STACK TICTOK"
-          song="high"
-          likes={123}
-          messages={40}
-          shares={32}
-        />
-        <Video
-          url="https://user-images.githubusercontent.com/53213397/103125812-b1cb5700-46cf-11eb-99a9-bf244cc2e52f.mp4"
-          channel="bbbb"
-          description="Happy new year"
-          song="Dynamite"
-          likes={710}
-          messages={321}
-          shares={120}
-        />
+        {videos.map(
+          ({ url, channel, song, likes, messages, description, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://tiktok-clone-dongjune.herokuapp.com/",
+  baseURL: "https://tiktok-clone-kdj.herokuapp.com/",
 });
 
 export default instance;
